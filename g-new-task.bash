29345014140530d352b290a2d6fc05f07b1f3119
Euclécio Josias <eucjosias@gmail.com>
2017-02-21 15:15:02 -0300

    Improvements; check if is a git repository

--- g-new-task.bash
+++ g-new-task.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 issue_name=$1
+
+if [ ! $(git rev-parse --is-inside-work-tree) ]; then
+    exit 1;
+fi
 repo_path=$(git config --get remote.origin.url | sed -r 's/(.*:)(.*)(\..*)/\2/')
 
 if [[ -z $1 ]] || [[ $1 == '--help' ]] || [[ $1 == '-h' ]]; then
@@ -16,19 +20,19 @@ fi
 
 if [ -z ${GITHUB_USER+x} ] || [ -z ${GITHUB_PASSWORD+x} ] && [ -z ${GITHUB_TOKEN+x} ]; then
     printf "If you want to move the task you should set the environment variables bellow:
+    \e[33mGITHUB_TOKEN\e[0m
+OR
     \e[33mGITHUB_USER
-    GITHUB_PASSWORD\e[0m
-    OR
-    \e[33mGITHUB_TOKEN\e[0m\n"
+    GITHUB_PASSWORD\e[0m\n"
 fi
 
-if [ -z ${GITHUB_TOKEN+x} ]; then
-    exists=$(curl -s -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/repos/compufour/compufacil/issues/$issue_name | grep message)
+if [ ! -z ${GITHUB_TOKEN+x} ]; then
+    issue_exists=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/compufour/compufacil/issues/$issue_name | grep message)
 else
-    exists=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/compufour/compufacil/issues/$issue_name | grep message)
+    issue_exists=$(curl -s -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/repos/compufour/compufacil/issues/$issue_name | grep message)
 fi
 
-if [[ $exists == *"Not Found"* ]]; then
+if [[ $issue_exists == *"Not Found"* ]]; then
     printf "\e[33mNo issue with this ID was found\e[0m\n"
     exit 1
 fi
@@ -44,15 +48,16 @@ git fetch origin
 git pull origin master
 git checkout -b $issue_name
 
-if [ -z ${GITHUB_TOKEN+x} ]; then
-    curl -s -X DELETE -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/repos/$repo_path/issues/$issue_name/labels/Stage%3A%20Analysis >/dev/null
-    curl -s -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/repos/$repo_path/issues/$issue_name/labels -d '["Stage: In Progress"]' >/dev/null
-else
+if [ ! -z ${GITHUB_TOKEN+x} ]; then
     curl -s -X DELETE -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$repo_path/issues/$issue_name/labels/Stage%3A%20Analysis >/dev/null
     curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$repo_path/issues/$issue_name/labels -d '["Stage: In Progress"]' >/dev/null
+else
+    curl -s -X DELETE -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/repos/$repo_path/issues/$issue_name/labels/Stage%3A%20Analysis >/dev/null
+    curl -s -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/repos/$repo_path/issues/$issue_name/labels -d '["Stage: In Progress"]' >/dev/null
 fi
 
-cpf-notify-slack "$USER created local branch $issue_name"
-cpf-notify-user "A new branch $issue_name was created in your workspace"
 
-cpf-metric "compufacil.new-task" 1
+#message="$USER created local branch $issue_name"
+#curl -X POST --data-urlencode \
+#    "payload={\"channel\": \"#devops\", \"username\": \"Notice \", \"text\": \"$message\", \"icon_emoji\": \":ghost:\"}" \
+#    https://hooks.slack.com/services/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
